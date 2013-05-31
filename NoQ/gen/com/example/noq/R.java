31610082efc014290dbc2e2ea7d1cd81d5f1f444
ipsita93 <ipsitamohapatra93@gmail.com>
2013-05-31 17:11:22 +0800

    another 3 activities added

--- NoQ/gen/com/example/noq/R.java
+++ NoQ/gen/com/example/noq/R.java
@@ -24,18 +24,27 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080008;
-        public static final int button1=0x7f080003;
-        public static final int button2=0x7f080004;
-        public static final int editText1=0x7f080005;
-        public static final int editText2=0x7f080006;
-        public static final int editText3=0x7f080007;
-        public static final int textView1=0x7f080000;
-        public static final int textView2=0x7f080001;
-        public static final int textView3=0x7f080002;
+        public static final int CheckBox2=0x7f08000d;
+        public static final int CheckBox3=0x7f08000c;
+        public static final int action_settings=0x7f08000e;
+        public static final int button1=0x7f080000;
+        public static final int button2=0x7f080002;
+        public static final int button3=0x7f080009;
+        public static final int button4=0x7f08000a;
+        public static final int checkBox1=0x7f08000b;
+        public static final int editText1=0x7f080003;
+        public static final int editText2=0x7f080007;
+        public static final int editText3=0x7f080008;
+        public static final int textView1=0x7f080004;
+        public static final int textView2=0x7f080006;
+        public static final int textView3=0x7f080001;
+        public static final int textView5=0x7f080005;
     }
     public static final class layout {
         public static final int main=0x7f030000;
+        public static final int receipt2=0x7f030001;
+        public static final int receipt3=0x7f030002;
+        public static final int receipts=0x7f030003;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- NoQ/src/com/example/noq/Main.java
+++ NoQ/src/com/example/noq/Main.java
@@ -9,6 +9,7 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
+// This is Receipt1 page
 public class Main extends Activity {
 
 	@Override
@@ -16,9 +17,9 @@ public class Main extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 		
-		Button b = (Button) findViewById(R.id.button1);
-	       b.setOnClickListener(new OnClickListener() {
-				
+		// to click on clear all button
+		Button b1 = (Button) findViewById(R.id.button1);
+	       b1.setOnClickListener(new OnClickListener() {
 				@Override
 				public void onClick(View arg0) {
 					EditText et1 = (EditText) findViewById(R.id.editText1);
@@ -29,7 +30,20 @@ public class Main extends Activity {
 					et3.setText("");
 				}
 			});
+	       
+	   	// to click on submit button
+	        final EditText et = (EditText) findViewById(R.id.editText3);
+	        Button b2 = (Button) findViewById(R.id.button2);
+	        b2.setOnClickListener(new OnClickListener() {		
+				@Override
+				public void onClick(View arg0) {
+					Intent intent = new Intent(Main.this, Receipts.class);
+					intent.putExtra("amount", et.getText());
+					startActivity(intent);
+				}
+			});
 	}
+	        
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
