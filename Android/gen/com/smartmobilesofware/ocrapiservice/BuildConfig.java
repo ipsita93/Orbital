abf6636c5b7920d676f71ac86321d8dce5dc93c7
ipsita93 <ipsitamohapatra93@gmail.com>
2013-07-07 12:58:28 +0800

    trial ocr done

---
+++ Android/gen/com/smartmobilesofware/ocrapiservice/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.smartmobilesofware.ocrapiservice;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
